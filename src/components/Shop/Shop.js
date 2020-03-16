847dcee9ff491f437e9c28a481eb936d43301405
sojib-bd <sojibrahmatuzzaman@gmail.com>
2020-03-17 02:29:48 +0600

    solving the NaN problem in shop.js

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -13,10 +13,22 @@ const Shop = () => {
 
     const addProductCount = (product) => {
         //console.log(product);
-        const newCart = [...cart, product];
+        console.log(cart)
+        const sameProduct = cart.find(x => x.key === product.key);
+        let count = 1;
+        let newCart;
+        if (sameProduct) {
+            count = sameProduct.quantity + 1;
+            sameProduct.quantity = sameProduct.quantity + 1;
+            const others = cart.filter(x => x.key !== product.key);
+            newCart = [...others, sameProduct]
+        }
+        else {
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+
         setCart(newCart)
-        const sameProduct = newCart.filter(x => x.key === product.key);
-        const count = sameProduct.length;
         addToDatabaseCart(product.key, count)
     }
     return (
